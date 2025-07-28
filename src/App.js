afe013dc0bf13a37314c26cc7d5846e19ff7c2db
AlexSalavat <alex.salavat88@gmail.com>
2025-07-28 18:50:55 +0300

    feat: обновление карточек

--- src/App.js
+++ src/App.js
@@ -1,45 +1,65 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./components/Home";
 import Showcase from "./components/Showcase";
 import Catalog from "./components/Catalog";
 import SupplierDetail from "./components/SupplierDetail";
 import LogisticsShowcase from "./components/LogisticsShowcase";
 import ProductionShowcase from "./components/ProductionShowcase";
+import TopProducers from "./components/TopProducers";
+import ProducerDetail from "./components/ProducerDetail";
 import NeirobizScreen from "./components/NeirobizScreen";
 import News from "./components/News";
 import Market from "./components/Market";
-import MarketSell from "./components/MarketSell";
-import MarketBuy from "./components/MarketBuy";
 import MarketSellCategory from "./components/MarketSellCategory";
-import MarketSellDetail from "./components/MarketSellDetail"; // <-- Новый!
+import MarketSellCreate from "./components/MarketSellCreate";
+import MarketSellDetail from "./components/MarketSellDetail";
+import MarketBuy from "./components/MarketBuy";
 import Profile from "./components/Profile";
+import CaviarWarBoard from "./components/CaviarWarBoard";
 import BottomNav from "./components/BottomNav";
-import TopProducers from "./components/TopProducers";
-import ProducerDetail from "./components/ProducerDetail";
 
-function App() {
+// Проверь, что ВСЕ указанные компоненты есть в src/components/ и экспортируются по default
+
+export default function App() {
   return (
     <Router>
       <div className="min-h-screen bg-black flex flex-col pb-20">
         <div className="flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
+
             <Route path="/catalog" element={<Showcase />} />
             <Route path="/catalog/suppliers" element={<Catalog />} />
             <Route path="/supplier/:id" element={<SupplierDetail />} />
+
             <Route path="/logistics" element={<LogisticsShowcase />} />
             <Route path="/production" element={<ProductionShowcase />} />
+
             <Route path="/producers" element={<TopProducers />} />
             <Route path="/producer/:id" element={<ProducerDetail />} />
+
             <Route path="/neirobiz" element={<NeirobizScreen />} />
+
             <Route path="/news" element={<News />} />
+            <Route path="/news/coast" element={<div style={{ color: "#fff", padding: 25 }}>Раздел в разработке</div>} />
+            <Route path="/news/ikra-wars" element={<CaviarWarBoard />} />
+            <Route path="/news/top-producers" element={<TopProducers />} />
+
             <Route path="/market" element={<Market />} />
-            <Route path="/market/sell" element={<MarketSell />} />
+            {/* Категории: /market/sell, /market/buy */}
+            <Route path="/market/sell" element={<MarketSellCategory />} />
+            <Route path="/market/sell/create" element={<MarketSellCreate />} />
+            <Route path="/market/sell/:id" element={<MarketSellDetail />} />
             <Route path="/market/buy" element={<MarketBuy />} />
-            <Route path="/market/sell/:category" element={<MarketSellCategory />} />
-            <Route path="/market/sell/:category/:id" element={<MarketSellDetail />} /> {/* Детали */}
+
             <Route path="/profile" element={<Profile />} />
+
+            {/* fallback на NotFound */}
+            <Route path="*" element={<div style={{
+              color: "#fff", background: "#000", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: 18
+            }}>Страница не найдена</div>} />
           </Routes>
         </div>
         <BottomNav />
@@ -47,5 +67,3 @@ function App() {
     </Router>
   );
 }
-
-export default App;

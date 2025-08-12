c271823c3b43e9d2200ea436fbb6ae807a18a11b
AlexSalavat <alex.salavat88@gmail.com>
2025-08-12 11:29:26 +0300

    chore: normalize line endings to LF and remove BOM

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,4 @@
-﻿/** @type {import('eslint').Linter.Config} */
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['react-app', 'react-app/jest', 'plugin:prettier/recommended'],
   rules: {

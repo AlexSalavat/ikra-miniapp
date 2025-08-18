07d08bb3b12d685f6f82f02f1300a0a6307a0701
AlexSalavat <alex.salavat88@gmail.com>
2025-08-18 12:03:07 +0300

    chore(lint/build): update eslint config, add .eslintignore; format build configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   root: true,
   env: { browser: true, es2021: true, node: true },
-  extends: ["react-app", "react-app/jest"],
+  extends: ['react-app', 'react-app/jest'],
   rules: {
     // временно: не роняем CI из-за форматирования
-    "no-console": "off"
-  }
+    'no-console': 'off',
+  },
 };
